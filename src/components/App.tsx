be5ca7fe606395fb2e20487991c7145828a7126e
Cam Keif <cameron.keif@gmail.com>
2020-10-19 17:39:42 -0500

    Add a title for the page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../styles/App.css';
+import bitcoinLogo from '../assets/bitcoin.svg';
 import CoinRows from './CoinRows';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
+      <h1>
+        C
+        <img src={bitcoinLogo} className="header-btc-logo" alt="bitcoin logo as an O" />
+        intracker
+      </h1>
       <CoinRows />
     </header>
     <footer>
