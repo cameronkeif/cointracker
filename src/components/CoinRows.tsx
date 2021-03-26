6b80e48e653830dc3344e493f165c06dc80e6292
Cam Keif <cameron.keif@gmail.com>
2021-03-26 17:10:52 -0500

    Refactor the coin add components into a separate composition component

--- src/components/CoinRows.tsx
+++ src/components/CoinRows.tsx
@@ -1,37 +1,24 @@
-import React, {
-  useEffect, useState, Fragment, ChangeEvent, KeyboardEvent,
-} from 'react';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import { green, grey } from '@material-ui/core/colors';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import Autocomplete, { AutocompleteRenderInputParams } from '@material-ui/lab/Autocomplete';
+import React, { useEffect, useState, Fragment } from 'react';
+
 import request from 'superagent';
 
+import AddCoinRow from './AddCoinRow';
 import CoinRow from './CoinRow';
 import api from '../api';
 import { CoinItem, CoinOption } from '../react-app-env';
 import spinnerIcon from '../assets/spinner.png';
 
-import coinOptions from '../data/coin-options.json';
-
 const CoinRows = () => {
   const [symbols, setSymbols] = useState<Set<string>>(new Set());
-  const [selectedCoin, setSelectedCoin] = useState<null | CoinOption>(null);
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [activeTimerId, setActiveTimerId] = useState<null | NodeJS.Timeout>(null);
 
-  const addCoin = () => {
-    if (selectedCoin === null) {
-      return;
-    }
-
+  const addCoin = (selectedCoin: CoinOption) => {
     const updatedSymbols = new Set(symbols);
     updatedSymbols.add(selectedCoin.symbol);
 
     setSymbols(updatedSymbols);
-    setSelectedCoin(null);
 
     if (activeTimerId) {
       clearInterval(activeTimerId);
@@ -79,7 +66,6 @@ const CoinRows = () => {
     />
   ));
 
-  const addButtonColor = selectedCoin ? green[500] : grey[500];
   return (
     <>
       <table className="coin-table">
@@ -95,44 +81,7 @@ const CoinRows = () => {
           {coinRows}
         </tbody>
       </table>
-      <div style={{
-        display: 'inline-flex', alignItems: 'center', width: 'auto', marginTop: 20,
-      }}
-      >
-        <Autocomplete
-          id="combo-box-demo"
-          options={coinOptions.filter((coinOption: CoinOption) => !symbols.has(coinOption.symbol))}
-          style={{ width: 300, margin: 'auto', display: 'inline-block' }}
-          renderInput={(params: AutocompleteRenderInputParams) => (
-            <TextField
-              {...params}
-              label="Add coin"
-              size="small"
-              variant="outlined"
-            />
-          )}
-          getOptionLabel={(coinOption: CoinOption) => `${coinOption.name} (${coinOption.symbol})`}
-          onChange={(
-            event: ChangeEvent<{}>,
-            newSelectedCoin: { name: string; symbol: string; } | null,
-          ) => { setSelectedCoin(newSelectedCoin); }}
-          onKeyDown={(event: KeyboardEvent<HTMLDivElement>) => {
-            if (event.key === 'Enter') {
-              addCoin();
-            }
-          }}
-          value={selectedCoin}
-        />
-        <IconButton
-          disabled={!selectedCoin}
-          onClick={addCoin}
-        >
-          <AddCircleIcon
-            style={{ color: addButtonColor, cursor: 'pointer' }}
-            fontSize="large"
-          />
-        </IconButton>
-      </div>
+      <AddCoinRow onAdd={addCoin} selectedSymbols={symbols} />
       {isLoading && symbols.size > 0 && <div><img src={spinnerIcon} className="loading" alt="Loading" /></div>}
     </>
   );
