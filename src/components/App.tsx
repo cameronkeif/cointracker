4d70e254dae6474cd66333d3d37872ed6270d8a9
Cameron Keif <cameron.keif@gmail.com>
2022-01-21 21:36:48 -0600

    farewell message :(

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,18 @@ const App: React.FC = () => (
         intracker
       </h1>
     </header>
+    <section className="farewall-message">
+      <div>
+        Regretfully, this application no longer works as the backing API requires a paid plan now.
+      </div>
+      <div>
+        This was a fun learning exercise regardless! Check out
+        {' '}
+        <a href="https://github.com/cameronkeif/cointracker">code</a>
+        {' '}
+        if you want to learn more about what I worked on here!
+      </div>
+    </section>
     <CoinRows />
     <footer>
       Data provided by Coinranking
